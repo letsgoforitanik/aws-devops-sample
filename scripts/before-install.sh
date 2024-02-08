68a14be3fea974223337699e3b851b3456b3740a
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2024-02-08 21:47:27 +0530

    Change buildspec and appspec

---
+++ scripts/before-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ACCESS_KEY=$(aws ssm get-parameter --name "/fibo-stress/access_key" --region "ap-south-1" --query "Parameter.Value")
+export SECRET_ACCESS_KEY=$(aws ssm get-parameter --name "/fibo-stress/secret_access_key" --region "ap-south-1" --query "Parameter.Value")
+export REGISTRY_USERNAME=$(aws ssm get-parameter --name "/fibo-stress/registry_username" --region "ap-south-1" --query "Parameter.Value")
+export IMAGE_REGISTRY=$(aws ssm get-parameter --name "/fibo-stress/image_registry_url" --region "ap-south-1" --query "Parameter.Value")
+
+aws configure set aws_access_key_id "$ACCESS_KEY" && aws configure set aws_secret_access_key "$SECRET_ACCESS_KEY"
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username "$REGISTRY_USERNAME" --password-stdin $IMAGE_REGISTRY
+docker pull $IMAGE_REGISTRY/fibo-stress:latest
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,3 @@
-echo Hello
+#!/bin/bash
+
+docker run -d -p 80:3000 $IMAGE_REGISTRY/fibo-stress:latest
