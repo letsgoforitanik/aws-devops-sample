d7faacc394985b91d637b57c8c541c3eeb6a03bf
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2024-02-09 05:20:36 +0530

    Added authorship

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -5,6 +5,5 @@ IMAGE_REGISTRY=$(aws ssm get-parameter --name "/fibo-stress/image_registry_url" 
 CODEBUILD_BUILD_NUMBER=$(aws ssm get-parameter --name "/fibo-stress/codebuild_build_number" --region ap-south-1 --query "Parameter.Value" | tr -d '"')
 
 aws ecr get-login-password --region ap-south-1 | docker login --username "$REGISTRY_USERNAME" --password-stdin $IMAGE_REGISTRY
-docker container stop $(docker container ls -q) && docker container prune -f 
 docker image rm -f $(docker image ls -q fibo-stress)
 docker pull $IMAGE_REGISTRY/fibo-stress:$CODEBUILD_BUILD_NUMBER
