a3c30355a72103e48bfcf85ba7af630ea66782de
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2024-02-09 04:53:33 +0530

    Initial Commit

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "$IMAGE_REGISTRY/fibo-stress:$CODEBUILD_BUILD_NUMBER" > /home/ubuntu/anik.txt
+
+IMAGE_REGISTRY=$(aws ssm get-parameter --name "/fibo-stress/image_registry_url" --region ap-south-1 --query "Parameter.Value" | tr -d '"')
+CODEBUILD_BUILD_NUMBER=$(aws ssm get-parameter --name "/fibo-stress/codebuild_build_number" --region ap-south-1 --query "Parameter.Value" | tr -d '"')
+
 docker run -d -p 3000:3000 $IMAGE_REGISTRY/fibo-stress:$CODEBUILD_BUILD_NUMBER
