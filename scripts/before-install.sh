4bbf57aa1be9f3381e5eef1b5672b525f07e69cd
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2024-02-09 03:26:37 +0530

    Change appspec

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-export ACCESS_KEY=$(aws ssm get-parameter --name "/fibo-stress/access_key" --region ap-south-1 --query "Parameter.Value")
-export SECRET_ACCESS_KEY=$(aws ssm get-parameter --name "/fibo-stress/secret_access_key"  --region ap-south-1 --query "Parameter.Value")
-export REGISTRY_USERNAME=$(aws ssm get-parameter --name "/fibo-stress/registry_username" --region ap-south-1 --query "Parameter.Value")
-export IMAGE_REGISTRY=$(aws ssm get-parameter --name "/fibo-stress/image_registry_url" --region ap-south-1 --query "Parameter.Value")
+export REGISTRY_USERNAME=$(aws ssm get-parameter --name "/fibo-stress/registry_username" --region ap-south-1 --query "Parameter.Value" | tr -d '"')
+export IMAGE_REGISTRY=$(aws ssm get-parameter --name "/fibo-stress/image_registry_url" --region ap-south-1 --query "Parameter.Value" | tr -d '"')
 
-aws ecr get-login-password --region ap-south-1 | docker login --username $REGISTRY_USERNAME --password-stdin $IMAGE_REGISTRY
+aws ecr get-login-password --region ap-south-1 | docker login --username "$REGISTRY_USERNAME" --password-stdin $IMAGE_REGISTRY
 docker pull $IMAGE_REGISTRY/fibo-stress
